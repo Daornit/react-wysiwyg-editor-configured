62b6a668455b264aa2683ec8e38f86362d66a7c3
Daornit <daornit2@gmail.com>
2020-05-03 17:08:19 +0800

    import statement fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Require Editor CSS files.
-import 'froala-editor/css/  .min.css';
+import 'froala-editor/css/froala_style.min.css';
 import 'froala-editor/css/froala_editor.pkgd.min.css';
 
 import FroalaEditorComponent from 'react-froala-wysiwyg';
